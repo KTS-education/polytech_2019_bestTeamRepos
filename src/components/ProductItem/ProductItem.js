f6c8d15c23feba9d79f9e2c685d16ab3ff891bf6
Darya <drmrhdt@gmail.com>
2019-10-16 17:25:51 +0300

    Add css to GiftsMyToMeContainer component

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -1,5 +1,6 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import classNames from "classnames";
 import "./ProductItem.css";
 
 class ProductItem extends React.Component {
@@ -10,18 +11,23 @@ class ProductItem extends React.Component {
             product_img_href: PropTypes.string.isRequired,
             product_price: PropTypes.number.isRequired,
             product_title: PropTypes.string.isRequired
-        })
+        }),
+        className: PropTypes.string
     };
+
+    static defaultProps = {
+        className: null
+    }
 
     render() {
         const {product_description, product_img_href, product_price, product_title} = this.props.product;
 
         return (
-            <div className="my-page-container">
+            <div className="product-item-container">
               <img className="item__image" src={product_img_href} alt={product_title}/>
               <h3>{product_title}</h3> 
               <p>{product_price}<span> &#8381;</span></p> 
-              <p>{product_description}</p>
+              <p className="item__description">{product_description}</p>
             </div>
         )
     }
