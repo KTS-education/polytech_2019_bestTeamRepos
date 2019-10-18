a00d2246ae35d37411e2f5caf9c86f6d6d42f9a1
nikita220800 <nikita220800@mail.ru>
2019-10-19 02:13:59 +0300

    add classname

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,27 +1,26 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Item from './Item';
-import './List.css';
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import Item from "./Item";
+import "./List.css";
 
 class List extends React.Component {
+  static propTypes = {
+    products: PropTypes.array.isRequired,
+    className: PropTypes.string
+  };
 
-    static propTypes = {
-        products: PropTypes.array.isRequired
-    };
+  render() {
+    const { products, className } = this.props;
 
-    render() {
-        const products = this.props.products;
-
-        return (
-            <ul className="products-list">
-                {
-                    products.map(product => {
-                        return <Item key={product.product_id} product={product} />
-                    })
-                }
-            </ul>
-        )
-    }
+    return (
+      <ul className={classNames("products-list", className)}>
+        {products.map(product => {
+          return <Item key={product.product_id} product={product} />;
+        })}
+      </ul>
+    );
+  }
 }
 
 export default List;
