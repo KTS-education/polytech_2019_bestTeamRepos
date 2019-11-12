33d898119e21025f27034d1d82bde1ade31472c1
nikita220800 <nikita220800@mail.ru>
2019-11-12 16:48:24 +0300

    header friends pictures

--- src/reducers/friends.js
+++
@@ -1,9 +0,0 @@
-import f1 from "@data/YourFriendsInfo/img/friend2.png";
-import f2 from "@data/YourFriendsInfo/img/friend1.png";
-import f3 from "@data/YourFriendsInfo/img/friend3.png";
-
-const initialState = {
-  friends: [f1, f2, f3]
-};
-
-export const friendsReducer = (state = initialState) => state;
