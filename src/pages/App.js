afe4d1af70b0010da0c92ca208980ecffe112723
Darya <drmrhdt@gmail.com>
2019-11-16 13:17:34 +0300

    Update App.js

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Routes from "@config/routes.js";
 import FriendList from "./FriendList";
@@ -11,17 +11,19 @@ import Header from "@components/Header";
 import "@constantcss/constants.css";
 import styles from "./App.module.scss";
 
-const App = props => {
-  return (
-    <div className={styles["app"]}>
-      <Header />
-      <Switch>
-        <Route exact path={Routes.mainPage} component={Main} />
-        <Route path={Routes.friendListPage} component={FriendList} />
-        <Route path={Routes.profile.path} render={props => <Profile />} />
-      </Switch>
-    </div>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <div className={styles["app"]}>
+        <Header />
+        <Switch>
+          <Route exact path={Routes.mainPage} component={Main} />
+          <Route path={Routes.friendListPage} component={FriendList} />
+          <Route path={Routes.profile.path} render={props => <Profile />} />
+        </Switch>
+      </div>
+    );
+  }
+}
 
 export default App;
