5ca17f604bce7f59714876ca38a69498014d3e1f
nikita220800 <nikita220800@mail.ru>
2019-11-18 20:07:38 +0300

    api tests

--- src/pages/App.js
+++ src/pages/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import { default as connectVK } from "@vkontakte/vk-connect";
 import Routes from "@config/routes.js";
 import FriendList from "./FriendList";
-import { callApi, api } from "@src/api.js";
+import { api } from "@src/api.js";
 import Main from "./Main";
 import Profile from "./Profile";
 
@@ -15,20 +15,31 @@ import styles from "./App.module.scss";
 class App extends Component {
   componentDidMount() {
     connectVK
-      .sendPromise("VKWebAppGetUserInfo", {
-        params: {
-          v: "5.103"
-        }
+      .sendPromise("VKWebAppGetAuthToken", {
+        app_id: 7186760,
+        scope: "friends, status"
       })
-      .then(response => {
-        console.log(response);
-        callApi(`/api/user/auth${window.location.search}`, "POST", {
-          body: response
-        }).then(result => {
-          result.response
-            ? console.log("err" + result.response)
-            : console.error(result.error);
-        });
+      .then(token => {
+        connectVK
+          .sendPromise("VKWebAppGetUserInfo", {
+            params: {
+              v: "5.103"
+            }
+          })
+          .then(response => {
+            response.auth_key = token.access_token;
+            console.log(response);
+            console.log(window.location.search);
+            api(
+              `/api/user/auth${window.location.search}`,
+              "POST",
+              response
+            ).then(result => {
+              result.response
+                ? console.log("err" + result.response)
+                : console.error(result.error);
+            });
+          });
       })
       .catch(error => console.log(error));
   }
