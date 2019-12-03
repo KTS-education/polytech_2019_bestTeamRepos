9ea8a3146d525393e8a8ea8775a6ce38144fa446
Daria <drmrhdt@mail.ru>
2019-12-03 20:16:23 +0300

    Fix long product's names

--- src/components/List/ListItem/Item/StatusButtons/StatusButtonsPopular/StatusButtonsPopular.js
+++ src/components/List/ListItem/Item/StatusButtons/StatusButtonsPopular/StatusButtonsPopular.js
@@ -3,11 +3,33 @@ import Button from "@components/Button";
 import styles from "./StatusButtonsPopular.module.scss";
 
 export default class StatusButtonsPopular extends Component {
+  // onClick = async () => {
+  //   const result = await api(`/api/wishlist/add`, "POST", {
+  //     id: product.Integer((required = True)),
+  //     price: product.Integer((required = True)),
+  //     name: product.String((required = True)),
+  //     photo: product.String((required = True)),
+
+  //     link: product.String(),
+  //     rating: product.Float(),
+  //     review_count: product.Integer(),
+  //     base_price: product.Integer(),
+  //     discount: product.Integer()
+  //   });
+
+  //   if (result) {
+  //     console.log(result);
+  //   } else {
+  //     console.log(result);
+  //   }
+  // };
+
   render() {
     return (
       <Button
         className={styles["item__button"]}
         children="Добавить в избранное"
+        onClick={this.onClick}
       />
     );
   }
