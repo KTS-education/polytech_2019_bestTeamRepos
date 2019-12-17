f885bbf393dc02a9a6046e45d95096489756b456
nikita220800 <nikita220800@mail.ru>
2019-12-17 20:04:08 +0300

    clear routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -4,8 +4,6 @@ export default {
   profile: {
     path: "/profile/:id",
     create: id => `/profile/${id}`,
-    // createWhatIwantPath: `/profile/:id/what-i-want`,
-    // createWhatIwant: id => `/profile/${id}/what-i-want`,
     createFromMePath: `/profile/:id/from-me`,
     createFromMe: id => `/profile/${id}/from-me`
   }
